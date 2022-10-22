c155f8ac94b85a1563aa4663a853b191b5e099bc
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-10-23 00:08:19 +0600

    дошёл до литерального типа ("2 стула");

--- training.js
+++ training.js
@@ -24,9 +24,93 @@ function lessonsCount(_a) {
     var lessons = _a.lessons;
     return lessons.length;
 }
-console.log(lessonsCount({ lessons: ['ak', 'sd'] }));
+console.log(lessonsCount({ lessons: ['ak', 'sd'] })); //2
 function lessonsCount2(_a) {
     var lessons = _a.lessons;
     return lessons.length;
 }
-console.log(lessonsCount2({ lessons: ['a', 'b', 'c', 'd', 'e'] }));
+console.log(lessonsCount2({ lessons: ['a', 'b', 'c', 'd', 'e', 'f'] })); // 6
+function sayHello(firstName, lastName) {
+    if (!lastName) {
+        return "Hi ".concat(firstName);
+    }
+    return "Hello ".concat(firstName, " ").concat(lastName);
+}
+console.log(sayHello('ayrat', 'sungatullin'));
+console.log(sayHello('ayrat'));
+/* Реализуйте функцию last() которая извлекает последний элемент из переданного значения.
+Значением может быть строка или число.
+Функция возвращает значение того же типа, которое было передано в качестве параметра    */
+function last(value) {
+    var variants = {
+        'string': function (value) {
+            return value[value.length - 1];
+        },
+        'number': function (value) {
+            if (!(value % 10)) {
+                return 0;
+            }
+            else {
+                return value % 10;
+            }
+        }
+    };
+    var type = typeof value;
+    return variants[type](value);
+}
+console.log(last('John'));
+console.log(last(1));
+console.log(last(134));
+function lastIndex(str, char) {
+    return str.lastIndexOf(char) >= 0 ? str.lastIndexOf(char) : null;
+}
+console.log(lastIndex('test', 't'));
+console.log(lastIndex('test', 'e'));
+console.log(lastIndex('test', 'p'));
+function formatPrice(num) {
+    if (num === void 0) { num = null; }
+    if (num === null || num === undefined) {
+        return '$0.00';
+    }
+    return "$".concat(num.toFixed(2));
+}
+console.log(formatPrice(3.145)); // '$3.15'
+console.log(formatPrice(200)); // '$200.00'
+console.log(formatPrice()); // '$0.00'
+console.log(formatPrice(null)); // '$0.00'
+var startGame = function () {
+    var state = ['turtle', null, null, null, null];
+    function makeTurn(move) {
+        var position = state.indexOf('turtle');
+        var dispatcher = {
+            'right': function () {
+                if (position + 1 >= state.length) {
+                    throw new Error('Вправо всё');
+                }
+                state[position] = null;
+                state[position + 1] = 'turtle';
+            },
+            'left': function () {
+                if (position - 1 < 0) {
+                    throw new Error('Влево всё');
+                }
+                state[position] = null;
+                state[position - 1] = 'turtle';
+            }
+        };
+        dispatcher[move]();
+    }
+    // END
+    return { makeTurn: makeTurn, state: state };
+};
+var _a = startGame(), makeTurn = _a.makeTurn, state = _a.state;
+console.log(state); // ['turtle', null, null, null, null]
+//makeTurn('left') // ERROR
+makeTurn('right');
+makeTurn('right');
+makeTurn('right');
+makeTurn('right');
+console.log(state); // [null, null, 'turtle', null, null]
+makeTurn('right');
+console.log(state); // [null, null, 'turtle', null, null]
+makeTurn('left');
--- training.ts
+++ training.ts
@@ -36,4 +36,112 @@ function lessonsCount2({lessons}:lessonsType):number {
     return lessons.length;
 }
 
-console.log(lessonsCount2({lessons:['a','b','c','d','e','f']})); // 5
+console.log(lessonsCount2({lessons:['a','b','c','d','e','f']})); // 6
+
+function sayHello (name:String):string;
+function sayHello (firstName:String, lastName:String):string;
+
+function sayHello (firstName:any, lastName?:any):string{
+    if(!lastName){
+        return `Hi ${firstName}`;
+    }
+    return `Hello ${firstName} ${lastName}`;
+}
+
+console.log(sayHello('ayrat', 'sungatullin'));
+console.log(sayHello('ayrat'));
+
+/* Реализуйте функцию last() которая извлекает последний элемент из переданного значения.
+Значением может быть строка или число.
+Функция возвращает значение того же типа, которое было передано в качестве параметра    */
+
+function last(value:unknown){
+    const variants:any = {
+        'string':function (value:string) {
+            return value[value.length - 1];
+        },
+        'number':function (value:number) {
+            if(!(value%10)){
+                return 0;
+            } else {
+                return value%10;
+            }
+        },
+    }
+    const type:any = typeof value;
+    return variants[type](value);
+}
+
+console.log(last('John'));
+console.log(last(1));
+console.log(last(134));
+
+type nullOrNum = null | Number;
+function lastIndex(str: string, char: string): nullOrNum {
+    return str.lastIndexOf(char) >= 0 ? str.lastIndexOf(char) : null;
+}
+
+console.log(lastIndex('test', 't'));
+console.log(lastIndex('test', 'e'));
+console.log(lastIndex('test', 'p'));
+
+function formatPrice(num:any=null){
+    if(num === null || num === undefined){
+        return '$0.00';
+    }
+    return `$${num.toFixed(2)}`
+}
+
+console.log(formatPrice(3.145)); // '$3.15'
+console.log(formatPrice(200)); // '$200.00'
+console.log(formatPrice()) // '$0.00'
+console.log(formatPrice(null)) // '$0.00'
+
+//------------Игра ход черепахи --------------------------------------
+type Turtle = 'turtle' | null;
+
+const startGame = () => {
+    const state: Array<Turtle> = ['turtle', null, null, null, null];
+
+    // BEGIN (write your solution here
+    type move = 'left' | 'right';
+    function makeTurn(move:move):void{
+        let position = state.indexOf('turtle');
+        const dispatcher = {
+            'right': function(){
+                if (position+1 >= state.length){
+                    throw new Error('Вправо всё');
+                }
+                state[position] = null;
+                state[position+1] = 'turtle';
+            },
+            'left': function(){
+                if (position-1 < 0){
+                    throw new Error('Влево всё');
+                }
+                state[position] = null;
+                state[position-1] = 'turtle';
+            }
+        }
+        dispatcher[move]();
+    }
+    // END
+
+    return { makeTurn, state };
+};
+
+const { makeTurn, state } = startGame();
+console.log(state); // ['turtle', null, null, null, null]
+
+//makeTurn('left') // ERROR
+
+makeTurn('right');
+makeTurn('right');
+makeTurn('right');
+makeTurn('right');
+console.log(state); // [null, null, 'turtle', null, null]
+makeTurn('right');
+console.log(state); // [null, null, 'turtle', null, null]
+makeTurn('left');
+
+// -----------------------------------------------
